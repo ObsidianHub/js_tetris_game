c460e5b5f3c50da2e7ce5c0a41b90fa7f32492a4
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-02-24 15:43:32 +0200

    decide a trouble in base consts (now ready to go)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const id = x => x;
 const add = x => y => x + y;
 const transpose = xsxs => xsxs[0].map((col, i) => xsxs.map(row => row[i]));
 const mirror = xsxs => xsxs.map(xs => xs.reverse());
-const pipe = (...fns) => x => [...fns].reduce((acc, f) => f(acc), x);
+const pipe = (...fs) => x => [...fs].reduce((acc, f) => f(acc), x);
 const map = f => xs => xs.map(f);
 const range = min => max => [...Array(max).keys()].map((_, i) => i + min);
 const k = x => _ => x;
@@ -15,7 +15,7 @@ const ifelse = c => t => f => x => (c(x) ? t(x) : f(x));
 const reduce = f => z => xs => xs.reduce((acc, x) => f(acc)(x), z);
 const eq = x => y => x == y;
 const find = f => xs => xs.find(f);
-const append = x => xs => [...xs, xs];
+const append = x => xs => [...xs, x];
 const not = f => x => !f(x);
 const and = x => y => x && y;
 const or = x => y => x || y;
@@ -29,14 +29,14 @@ const prop = p => o => o[p];
 const both = f => g => x => f(x) && g(x);
 
 const Color = {};
-Color.black = s => `\x1b[30m${s}\x1b[om`;
-Color.red = s => `\x1b[31m${s}\x1b[om`;
-Color.green = s => `\x1b[32m${s}\x1b[om`;
-Color.yellow = s => `\x1b[33m${s}\x1b[om`;
-Color.blue = s => `\x1b[34m${s}\x1b[om`;
-Color.magenta = s => `\x1b[35m${s}\x1b[om`;
-Color.cyan = s => `\x1b[36m${s}\x1b[om`;
-Color.white = s => `\x1b[37m${s}\x1b[om`;
+Color.black = s => `\x1b[30m${s}\x1b[0m`;
+Color.red = s => `\x1b[31m${s}\x1b[0m`;
+Color.green = s => `\x1b[32m${s}\x1b[0m`;
+Color.yellow = s => `\x1b[33m${s}\x1b[0m`;
+Color.blue = s => `\x1b[34m${s}\x1b[0m`;
+Color.magenta = s => `\x1b[35m${s}\x1b[0m`;
+Color.cyan = s => `\x1b[36m${s}\x1b[0m`;
+Color.white = s => `\x1b[37m${s}\x1b[0m`;
 
 const Pieces = {
   I: [
